32c58940d85770f165faac14694c5f078887bf68
reffobic <133569987+reffobic@users.noreply.github.com>
2023-09-22 01:14:12 +0300

    Finished the main code no testing yet

--- main.cpp
+++ main.cpp
@@ -2,12 +2,38 @@
 // Created by Mohamed El Refai on 22/9/2023
 //
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << std::endl;
+    srand(time(0));
+
+    int randomNumber = rand() % 100 + 1;
+    int guess, attempts = 0;
+
+    cout << "---------- Guess the Number Game ----------" << endl;
+    cout << "A number have been picked from 1 to 100, try to guess it ;)" << endl;
+
+    do {
+        cout << "Enter your guess: "
+        cin >> guess;
+
+        if (guess < 0 && guess > 100){
+            cout << "Invalid input, please enter a number between 1 and 100" << endl;
+            continue;
+        }
+
+        attempts++;
+
+        if(guess < randomNumber){
+            cout << "Too low, try again :)" << endl;
+        } else if (guess > randomNumber){
+            cout << "Too high, try again :)" << endl;
+        } else {
+            cout << "Nice you got it right! Here is your free cookie :D" << endl;
+        }
+    } while (guess != randomNumber);
+
     return 0;
 }
