39a0a9c3e8d09da2acee4f9460b2e26dadfeadbd
reffobic <133569987+reffobic@users.noreply.github.com>
2023-09-22 01:04:47 +0300

    Include libraries and name comment

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+//
+// Created by Mohamed El Refai on 22/9/2023
+//
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
+
 int main() {
     cout << "Hello, World!" << std::endl;
     return 0;
